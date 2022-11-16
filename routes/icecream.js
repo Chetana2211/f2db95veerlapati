bd593945a12b5e4a609ebef54c8eb3f1d1aa501f
Chetana2211 <108295579+Chetana2211@users.noreply.github.com>
2022-11-16 16:53:36 -0600

    added create

--- routes/icecream.js
+++ routes/icecream.js
@@ -2,12 +2,13 @@ var express = require('express');
 const icecream_controlers= require('../controllers/icecream'); 
 var router = express.Router(); 
  
-/* GET costumes */ 
+/* GET icecreams */ 
 router.get('/', icecream_controlers.icecream_view_all_Page ); 
 
 /* GET detail icecream page */ 
 router.get('/detail', icecream_controlers.icecream_view_one_Page); 
 
-
+/* GET create icecream page */ 
+router.get('/create', icecream_controlers.icecream_create_Page);
 
 module.exports = router; 
