f98bb28e872257830529b55514788f42de192672
Chetana2211 <108295579+Chetana2211@users.noreply.github.com>
2022-11-30 17:51:32 -0600

    added buttons

--- models/icecream.js
+++ models/icecream.js
@@ -1,8 +1,23 @@
 const mongoose = require("mongoose") 
 const icecreamSchema = mongoose.Schema({ 
- icecream_type: String, 
- size: String, 
- cost: Number 
-}) 
+icecream_type: {
+    type:String,
+    required:["Icecream name is required."],
+    required:["Icecream name cannot be empty."]
+ }, 
+ size:  {
+    type:String,
+    required:["Icecream size is required."],
+    required:["Icecream size cannot be empty."]
+ },
+ cost:{
+    type: Number,
+    min:[5,"Cost Should be minimum of 5$ "],
+    max:[50,"Cost Cannot be greater than 50$ "],
+    required:["Icecream cost cannot be empty."]
+}
+})
 
 module.exports = mongoose.model("icecreams", icecreamSchema) 
+
+ 
